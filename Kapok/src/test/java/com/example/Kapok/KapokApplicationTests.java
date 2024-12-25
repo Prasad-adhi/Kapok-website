8822d142ebfae634b4b7dce81e55a6b99dcb73a2
Prasad-adhi <prasad.adhi08@gmail.com>
2024-12-25 16:51:58 -0500

    First commit

---
+++ Kapok/src/test/java/com/example/Kapok/KapokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Kapok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KapokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
