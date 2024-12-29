62845e129eff2b4ee1c65df668a7a3c04a496541
Prasad-adhi <prasad.adhi08@gmail.com>
2024-12-29 13:19:22 -0500

    mutation-insert

--- Kapok/src/main/java/com/example/Kapok/DonarController.java
+++ Kapok/src/main/java/com/example/Kapok/DonarController.java
@@ -3,6 +3,7 @@ package com.example.Kapok;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
@@ -18,5 +19,10 @@ public class DonarController {
     @QueryMapping
     public Optional<Donar> donarbyID(@Argument Integer id) {
         return Donar.getdonarById(id);
+    }
+    
+    @MutationMapping
+    public boolean addDonar(@Argument Integer id, @Argument String fName, @Argument String lName, @Argument String Email, @Argument String Pwd) {
+    	return Donar.addDonar(id, fName, lName, Email, Pwd);
     }
 }
